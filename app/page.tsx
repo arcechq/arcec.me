199f043d2fba9c914e68127b068aed200b441fac
Aditya Roychoudhary <ar006985@gmail.com>
2026-06-06 16:45:07 +0530

    add: base colours and metadata changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <>
-      <p className="text-3xl font-bold underline">Hello Next!</p>
-    </>
-  );
+    <main className="min-w-screen min-h-screen bg-background">
+      <div className="p-10 ">
+        <h1 className="text-4xl font-bold text-foreground font-sans">arcec.me</h1>
+        <p className="text-muted">Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+      </div>
+    </main>
+  )
 }
